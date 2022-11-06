40c5d5783fd1aaa2e4fc351423ef81c10412520f
Nick Martinez <nam0420@tamu.edu>
2022-11-05 19:25:20 -0500

    Ejs files set up and css working with them
    Co-authored-by: SyahImran <SyahImran@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+// const { appendFile } = require('fs');
+// const http = require('http');
+const express = require('express');
+const app = express();
+const hostname = 'localhost';
 const port = 3000;
 
-//Create a server object and pass an arrow function
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end("hello world");
+// Create a server object and pass an arrow function
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','text/plain');
+//     res.end("hello world");
+// });
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:${port}/');
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/entree', (req, res) => {
+    res.render('entree');
+});
+app.get('/manager', (req, res) => {
+    res.render('manager');
+});
+app.get('/starter', (req, res) => {
+    res.render('starter');
+});
+app.get('/topping', (req, res) => {
+    res.render('topping');
+});
+app.get('/placeorder', (req, res) => {
+    res.render('placeorder');
+});
+
+//app.use('/public', express.static('public'));
+
+app.listen(port, hostname, () => {
+    console.log('Server running at http://localhost:${port}/');
 });
