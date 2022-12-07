e6460a473165fc7387e7974d03a54b58ead2c36f
Nick Martinez <nam0420@tamu.edu>
2022-12-06 19:17:46 -0600

    Translate Setup and implementation started
    Co-authored-by: NebiyouErsabo <NebiyouErsabo@users.noreply.github.com>
    Co-authored-by: SyahImran <SyahImran@users.noreply.github.com>

---
+++ public/js/google_translate.js
@@ -0,0 +1,34 @@
+const { json } = require('express');
+
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config();
+
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+const translate =new Translate({
+    credentials:CREDENTIALS,
+    projectId: CREDENTIALS.projectId
+});
+const testTrans = async(text, language)=>{
+    if(language != undefined && language !='en'){
+        return translateText(text,language);
+    }
+    return text;
+};
+const translateText = async(text,targetLanguage)=>{
+    try{
+        let [response] = await translate.translate(text, targetLanguage);
+        return response;
+    }catch(error){
+        console.log('Error translating text: ${error}');
+        return 0;
+    }
+};
+
+translateText('Hello World!','es-MX')
+    .then((res)=>{
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
